bbaf3433a5cfb9c67dfa1504541a34d2f9e916ff
Meierhoff, Michael <michael.meierhoff@ddd-journey.de>
2022-02-27 21:22:04 +0100

    First steps. Modify Home Page to Minisign-WEB PoC and and add a file upload conponent.

---
+++ frontend/src/components/MainContainer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For the Project Repository,<br>
+      check out the
+      <a href="https://github.com/DDD-Journey/minisign-web-poc" target="_blank" rel="noopener">Github Project of the Minisign-Web PoC</a>.
+    </p>
+    <h3>First Vue Component to Upload a file</h3>
+    <file-upload />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import FileUpload from "@/components/FileUpload.vue";
+
+@Options({
+  components: {
+    FileUpload,
+  },
+  props: {
+    msg: String
+  }
+})
+export default class HelloWorld extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+a {
+  color: #42b983;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <MainContainer msg="Welcome to Minisign-Web PoC"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import MainContainer from '@/components/MainContainer.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    MainContainer,
   },
 })
 export default class HomeView extends Vue {}
