5463986523eda9f9b7c40fb77dbf338a82ff9925
Markus Hanses <markus.hanses@ddd-journey.de>
2022-02-27 16:29:29 +0100

    SpringBoot project initially created.

---
+++ backend/src/main/java/org/dddjourney/minisignpocbackend/MinisignPocBackendApplication.java
@@ -0,0 +1,13 @@
+package org.dddjourney.minisignpocbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinisignPocBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinisignPocBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/dddjourney/minisignpocbackend/MinisignPocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dddjourney.minisignpocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinisignPocBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
