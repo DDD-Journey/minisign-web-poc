06f25db90f63a1d02fbd88d40635b89d9642358c
Markus hanses <hanses.markus@gmail.com>
2022-04-01 21:39:03 +0200

    Cors and CSRF deactivated.

---
+++ backend/src/main/java/org/dddjourney/minisignpocbackend/infrastructure/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package org.dddjourney.minisignpocbackend.infrastructure.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+    }
+
+}
